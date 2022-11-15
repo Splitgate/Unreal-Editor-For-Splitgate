bd1014cd2a0b9f43c6458181b7f94bf87f2d7c55
wooby <pea@dr.com>
2022-11-15 11:30:17 +0000

    removed adams custom pak folder

--- Accustomed_Packages/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-echo Building pak...
-u4pak.exe pack pakchunk65-WindowsNoEditor.pak PortalWars
-u4pak.exe list pakchunk65-WindowsNoEditor.pak
-u4pak.exe info pakchunk65-WindowsNoEditor.pak
-u4pak.exe check pakchunk65-WindowsNoEditor.pak
-echo Built pak!
-pause >nul
