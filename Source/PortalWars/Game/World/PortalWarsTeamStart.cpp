dc982e00369863b9d82154b3e685cfe0de186a67
Adam <63616530+NotFakeAdam@users.noreply.github.com>
2022-11-22 15:55:51 +0000

    Made pickups have a visual flare and gave a visual change to the team starts!!!!

--- Source/PortalWars/Game/World/PortalWarsTeamStart.cpp
+++ Source/PortalWars/Game/World/PortalWarsTeamStart.cpp
@@ -3,3 +3,28 @@
 
 #include "PortalWarsTeamStart.h"
 
+void APortalWarsTeamStart::OnConstruction(const FTransform& Transform)
+{
+	GetGoodSprite()->ScreenSize = 0.5f;
+	GetGoodSprite()->bIsScreenSizeScaled = true;
+
+	if (SpawnTeam == 0)
+	{
+		GetGoodSprite()->SetSprite(AmbientTeamTexture);
+	}
+
+	if (SpawnTeam == 1)
+	{
+		GetGoodSprite()->SetSprite(BlueTeamTexture);
+	}
+
+	if (SpawnTeam == 2)
+	{
+		GetGoodSprite()->SetSprite(RedTeamTexture);
+	}
+
+	if (SpawnTeam >= 3)
+	{
+		GetGoodSprite()->SetSprite(AmbientTeamTexture);
+	}
+}
--- Source/PortalWars/Game/World/PortalWarsTeamStart.h
+++ Source/PortalWars/Game/World/PortalWarsTeamStart.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerStart.h"
+#include "Engine/Texture2D.h"
+#include "Components/BillboardComponent.h"
+
 #include "PortalWarsTeamStart.generated.h"
 
 /**
@@ -16,6 +19,8 @@ class PORTALWARS_API APortalWarsTeamStart : public APlayerStart
 	
 public:
 
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Team Settings")
 	int32 SpawnTeam;
 
@@ -27,4 +32,8 @@ public:
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Team Settings")
 	uint8 bInitialSpawn : 1;
+
+	UTexture2D* BlueTeamTexture = (UTexture2D*)StaticLoadObject(UTexture2D::StaticClass(), this, TEXT("/Game/UI/HUD/Indicators/Textures/indicator-pointer-blue.indicator-pointer-blue"));
+	UTexture2D* RedTeamTexture = (UTexture2D*)StaticLoadObject(UTexture2D::StaticClass(), this, TEXT("/Game/UI/HUD/Indicators/Textures/indicator-pointer-red.indicator-pointer-red"));
+	UTexture2D* AmbientTeamTexture = (UTexture2D*)StaticLoadObject(UTexture2D::StaticClass(), this, TEXT("/Game/UI/HUD/Indicators/Textures/indicator-pointer-white.indicator-pointer-white"));
 };
